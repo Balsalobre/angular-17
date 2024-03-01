20f7a36ee9fa83a250bea1bff18c1f48232c6fd0
balsalobre <carlosocb@gmail.com>
2024-03-01 18:43:40 +0100

    AfterRender and AfterNextRender hooks

--- src/app/domain/products/products.component.ts
+++ src/app/domain/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, afterNextRender, afterRender, computed, viewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -6,8 +6,19 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterLink],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
+  productImage = viewChild.required<ElementRef<HTMLImageElement>>('productImage');
+  imageSrc = computed(() => this.productImage()?.nativeElement.src);
 
+  constructor() {
+    afterRender(() => {
+      console.log('Product Image afterRender src', this.productImage()?.nativeElement.src);
+    });
+
+    afterNextRender(() => {
+      console.log('Product Image afterNextRender src', this.productImage()?.nativeElement.src);
+    });
+  }
 }
